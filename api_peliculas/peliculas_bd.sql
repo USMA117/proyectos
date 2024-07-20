6562b89ef3495431c2a5c647aae68dd340aaed8c
Sebastian Usma <johan58usma@gmail.com>
2024-07-20 12:53:43 -0500

    ADD: Interfaz de registro

--- api_peliculas/peliculas_bd.sql
+++ api_peliculas/peliculas_bd.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS peliculas_bd;
 CREATE DATABASE peliculas_bd;
 USE peliculas_bd;
 
@@ -7,6 +8,7 @@ CREATE TABLE peliculas (
     titulo VARCHAR(255) NOT NULL,
     duracion INT NOT NULL,
     elenco TEXT NOT NULL,
+    url_trailer VARCHAR(255) NOT NULL,
     categoria VARCHAR(50) NOT NULL,
     descripcion TEXT NOT NULL
 );
@@ -17,91 +19,101 @@ CREATE TABLE lista (
     FOREIGN KEY(pelicula_id) REFERENCES peliculas (id_pelicula)
 );
 
-INSERT INTO peliculas (url_portada, titulo, duracion, elenco, categoria, descripcion)
+INSERT INTO peliculas (url_portada, titulo, duracion, elenco,url_trailer, categoria, descripcion)
 VALUES ('https://resizing.flixster.com/RxGReKiNeLRDhz_1DDe-JiurSSY=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p7825626_p_v8_ae.jpg', 
     'Inception', 
     148, 
-    'Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page', 
+    'Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page',
+    'https://www.youtube.com/watch?v=YoHD9XEInc0',
     'Sci-Fi', 
     'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.');
 
-INSERT INTO peliculas (url_portada, titulo, duracion, elenco, categoria, descripcion)
+INSERT INTO peliculas (url_portada, titulo, duracion, elenco, url_trailer, categoria, descripcion)
 VALUES 
 ('https://resizing.flixster.com/hgcQ8juhhUYQofbQ6wsXfhicb3Y=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p15987_p_v13_ap.jpg', 
     'The Shawshank Redemption', 
     142, 
-    'Tim Robbins, Morgan Freeman, Bob Gunton', 
+    'Tim Robbins, Morgan Freeman, Bob Gunton',
+    'https://www.youtube.com/watch?v=PLl99DlL6b4', 
     'Drama', 
     'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.');
 
-INSERT INTO peliculas (url_portada, titulo, duracion, elenco, categoria, descripcion)
+INSERT INTO peliculas (url_portada, titulo, duracion, elenco, url_trailer, categoria, descripcion)
 VALUES 
 ('https://resizing.flixster.com/aLKvx7GjWgFMOfboq5KNOX1kCCg=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p6326_p_v13_be.jpg', 
     'The Godfather', 
     175, 
-    'Marlon Brando, Al Pacino, James Caan', 
+    'Marlon Brando, Al Pacino, James Caan',
+    'https://www.youtube.com/watch?v=UaVTIH8mujA', 
     'Crime', 
     'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.');
 
-INSERT INTO peliculas (url_portada, titulo, duracion, elenco, categoria, descripcion)
+INSERT INTO peliculas (url_portada, titulo, duracion, elenco, url_trailer, categoria, descripcion)
 VALUES 
 ('https://resizing.flixster.com/dJM7TJzf7qEp9NA2Kni0Cug9myc=/206x305/v2/https://resizing.flixster.com/Wg25mLoPWxjcxXzNyaSF4VGgGE4=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2ZiNjZiNWFkLWVhNzEtNDRhMC1iNGIwLTFmY2FkNzllNTJlMi5qcGc=', 
     'The Dark Knight', 
     152, 
-    'Christian Bale, Heath Ledger, Aaron Eckhart', 
+    'Christian Bale, Heath Ledger, Aaron Eckhart',
+    'https://www.youtube.com/watch?v=_PZpmTj1Q8Q',
     'Action', 
     'When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.');
 
-INSERT INTO peliculas (url_portada, titulo, duracion, elenco, categoria, descripcion)
+INSERT INTO peliculas (url_portada, titulo, duracion, elenco, url_trailer, categoria, descripcion)
 VALUES 
 ('https://resizing.flixster.com/bwK3C8Xd98Mfs6Z7gYfH-wp6QHI=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p15684_p_v12_ak.jpg', 
     'Pulp Fiction', 
     154, 
     'John Travolta, Uma Thurman, Samuel L. Jackson', 
+    'https://www.youtube.com/watch?v=tGpTpVyI_OQ',
     'Crime', 
     'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.');
 
-INSERT INTO peliculas (url_portada, titulo, duracion, elenco, categoria, descripcion)
+INSERT INTO peliculas (url_portada, titulo, duracion, elenco, url_trailer, categoria, descripcion)
 VALUES 
 ('https://resizing.flixster.com/zZCVxTj9dWokzMMyqzV63YcNDHk=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/NowShowing/3509/3509_aa.jpg', 
     'Forrest Gump', 
     142, 
-    'Tom Hanks, Robin Wright, Gary Sinise', 
+    'Tom Hanks, Robin Wright, Gary Sinise',
+    'https://www.youtube.com/watch?v=bLvqoHBptjg', 
     'Drama', 
     'The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other historical events unfold from the perspective of an Alabama man with an IQ of 75.');
 
-INSERT INTO peliculas (url_portada, titulo, duracion, elenco, categoria, descripcion)
+INSERT INTO peliculas (url_portada, titulo, duracion, elenco, url_trailer, categoria, descripcion)
 VALUES 
 ('https://resizing.flixster.com/C9FzmCCRIYz2I-F3T2wZgJqDDzc=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p23069_p_v13_au.jpg', 
     'Fight Club', 
     139, 
     'Brad Pitt, Edward Norton, Meat Loaf', 
+    'https://www.youtube.com/watch?v=BdJKm16Co6M',
     'Drama', 
     'An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.');
 
-INSERT INTO peliculas (url_portada, titulo, duracion, elenco, categoria, descripcion)
+INSERT INTO peliculas (url_portada, titulo, duracion, elenco, url_trailer, categoria, descripcion)
 VALUES 
 ('https://resizing.flixster.com/kO9s-jGsOi3YXyHkzVlmO9Z5lzI=/206x305/v2/https://resizing.flixster.com/hTz9Ap43sCkvDiFvCkjmb1IWkUg=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2EwMGEwNmQxLTE1MGYtNGQwYS04ZDhlLWQ0MzYwOTQ5M2JlMC5qcGc=', 
     'The Matrix', 
     136, 
-    'Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss', 
+    'Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss',
+    'https://www.youtube.com/watch?v=vKQi3bBA1y8', 
     'Sci-Fi', 
     'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.');
 
-INSERT INTO peliculas (url_portada, titulo, duracion, elenco, categoria, descripcion)
+INSERT INTO peliculas (url_portada, titulo, duracion, elenco, url_trailer, categoria, descripcion)
 VALUES 
 ('https://resizing.flixster.com/7c3qnZfPzZgID7Ft97PccFwEf9U=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p10543523_p_v8_as.jpg', 
     'Interstellar', 
     169, 
-    'Matthew McConaughey, Anne Hathaway, Jessica Chastain', 
+    'Matthew McConaughey, Anne Hathaway, Jessica Chastain',
+    'https://www.youtube.com/watch?v=zSWdZVtXT7E', 
     'Sci-Fi', 
     'A team of explorers travel through a wormhole in space in an attempt to ensure humanity survival.');
 
-INSERT INTO peliculas (url_portada, titulo, duracion, elenco, categoria, descripcion)
+INSERT INTO peliculas (url_portada, titulo, duracion, elenco, url_trailer, categoria, descripcion)
 VALUES 
 ('https://resizing.flixster.com/NOThcrrGmuUx6I5WNDq3oGqXbjc=/206x305/v2/https://resizing.flixster.com/u_a3AuTtJEV_zBm9hwr9kbU19F8=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2Y4ODg4N2FiLTAwYmYtNDMyMS05NWQ2LTIwMTA4ZmMyOTgzMy5qcGc=', 
     'The Lion King', 
     88, 
     'Matthew Broderick, Jeremy Irons, James Earl Jones', 
+    'https://www.youtube.com/watch?v=lFzVJEksoDY',
     'Animation', 
     'Lion prince Simba and his father are targeted by his bitter uncle, who wants to ascend the throne himself.');
---
+++ api_peliculas/register.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- <title>Api videogames</title> -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="./WEB/css/style.css">
+    </head>
+    <body>
+        <div class="contenedor_principal bg-dark">
+            <div class="row text-white py-4 rounded" style="width:400px; background: rgba(248, 248, 248, 0.753);">
+                <!-- <div class="col-12 bg-danger mt-0">
+                    <h4 class="fw-bold">REGISTER</h4>
+                </div> -->
+                <div class="col-12 mt-4">
+                    <form action="">
+                        <div class="row justify-content-start  text-center">
+                            <div class="col-12 mb-3">
+                                <label for="" class="bg-danger">Username</label>
+                                <input name="username" type="text">
+                            </div>
+                            <div class="col-12 mb-3">
+                                <label for="" class="label-correo bg-danger mr-0">Email address</label>
+                                <input type="email" >
+                            </div>
+                            <div class="col-12 mb-3">
+                                <label for="" class="bg-danger">Password</label>
+                                <input type="password">
+                            </div>
+                            <div class="col-12 mt-4">
+                                <button class="btn btn-outline-danger px-5">REGISTER</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+</html>
